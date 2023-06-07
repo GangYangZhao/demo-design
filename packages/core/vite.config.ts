ed0cce8aadf18b9ebbd6dd3f958262ae97528483
GangYangZhao <zhaogangyang@ones.ai>
2023-06-07 22:58:33 +0800

    feat: temp save

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteEslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteEslint(), react()],
 });
